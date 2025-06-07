749658c102e321db331900af81083cfdb413c3c4
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-08 06:24:21 +0700

    feat: Create example listener

--- src/main/java/io/github/mcengine/dlc/example/DLCListener.java
+++ src/main/java/io/github/mcengine/dlc/example/DLCListener.java
@@ -1,6 +1,12 @@
 package io.github.mcengine.dlc.example.listener;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 /**
@@ -11,7 +17,19 @@ public class DLCListener implements Listener {
 
     public DLCListener(Plugin plugin) {
         this.plugin = plugin;
+        // Register the listener
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    // Add your event handlers here
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.GREEN + "Welcome to the server, " + player.getName() + "!");
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Bukkit.getLogger().info(player.getName() + " has left the server.");
+    }
 }
